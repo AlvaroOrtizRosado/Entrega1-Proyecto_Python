98fd3468cc62e973685e5ddc3c81f82efa1648c2
AlvaroOrtizRosado <114604271+AlvaroOrtizRosado@users.noreply.github.com>
2022-11-20 20:55:41 +0100

    Add files via upload

--- scr/coches.py
+++ scr/coches.py
@@ -1,14 +1,11 @@
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 import csv
 from datetime import datetime
 
 coche = namedtuple('coche','id,marca,color,fecha_matriculacion,averiado,kilometros,propietarios_anteriores,características,numero_bastidor')
 
-
-
 def parsea_fecha(cadena):
     return datetime.strptime(cadena, "%d/%m/%Y")
-
 
 def parsea_boolean(cadena):
     if cadena == "VERDADERO":
@@ -17,7 +14,6 @@ def parsea_boolean(cadena):
         return False
         
 def lee_coches(info_coche):
-    #res = list
     res = []
     with open(info_coche,encoding='utf-8') as f:
         lector = csv.reader(f, delimiter=';')
@@ -27,4 +23,43 @@ def lee_coches(info_coche):
             res.append(tupla)
     return res
 
+def número_marcas_distintas(registros):
+    res=set()
+    for r in registros:
+        res.add(r.marca)
+    return len(res)
 
+def media_kilómetros(registros):
+    res=list()
+    if len(registros)>0:
+        for r in registros:
+            res.append(r.kilometros)
+        med_km=sum(res)/len(res)
+    return med_km  
+
+def max_kilómetros(registros, matriculación):
+    res = dict()
+    for r in registros:
+        if matriculación == True :
+            clave = r.kilometros
+            if clave not in res:
+                res[clave]=0
+            res[clave]+=r.kilometros
+    return max(res.items(),key=lambda e:e[1])[0]
+
+def marcas_con_menos_averías(registros, n=3):
+    res = dict()
+    for r in registros:
+        clave=r.marca
+        if clave not in res:
+            res[clave]=0
+        res[clave]+=r.averiado
+    return sorted(res.items(),
+                 key=lambda e:e[1], 
+                 reverse=False)[:n]
+
+def agrupar_por_color(registros):
+    res=defaultdict(list)
+    for r in registros:
+        res[r.color].append(r)
+    return res
--- scr/coches_test.py
+++ scr/coches_test.py
@@ -5,7 +5,33 @@ def test_lee_coches():
     print("Los 3 primeros registros son:",datos[:3])
     print("Los 3 útimos registros son:",datos[-3:])
 
+def test_número_marcas_distintas():
+     print ("Hay {} marcas distintas en los datos"
+           .format(número_marcas_distintas(datos)))
+
+def test_media_kilómetros():
+    media = media_kilómetros(datos)
+    print(f"La media de los kilómetros recorridos es {int(media)}.")
+
+def test_max_kilómetros():
+    print("EL coche matriculado con mas kilómetros ha recorrido {} kilómetros."
+        .format(int(max_kilómetros(datos, True))))
+
+def test_marcas_con_menos_averías(n=5):
+    print("\n Las {} marcas con menos averías son:".
+            format(n))
+    for c, v in marcas_con_menos_averías(datos,n):
+        print("{} ==> {}".format(c,v))
+
+def test_agrupar_por_color(color, n=5):
+    print(agrupar_por_color(datos)[color][:n])
+
+#Importante
 datos = lee_coches("Proyecto Python/data/coches_50.csv")
 
-if __name__ == '__main__':
-    test_lee_coches()
+#test_lee_coches()
+#test_número_marcas_distintas()
+#test_media_kilómetros()
+#test_max_kilómetros()
+#test_marcas_con_menos_averías()
+#test_agrupar_por_color("Green")
